12955f47e1ee594999664436464e383bb1bf907c
n-eri <n.eri@metaphase.co.jp>
2023-09-03 18:02:12 +0900

    アプリのindexを調整

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { json } from "@remix-run/node";
 import {
   useActionData,
@@ -18,9 +18,16 @@ import {
   Divider,
   List,
   Link,
+  Badge,
+  TextField,
+  Spinner,
+  MediaCard,
 } from "@shopify/polaris";
 
 import { authenticate } from "../shopify.server";
+import { _getShopFromQuery, _getAdminFromShop } from "../my_util";
+import { useAppBridge } from '@shopify/app-bridge-react';
+import { authenticatedFetch } from "@shopify/app-bridge-utils";
 
 export const loader = async ({ request }) => {
   const { session } = await authenticate.admin(request);
@@ -95,17 +102,82 @@ export default function Index() {
 
   const generateProduct = () => submit({}, { replace: true, method: "POST" });
 
+  // const app = useAppBridge();
+
+  // const shop = _getShopFromQuery(window);
+
+  const [meta, setMeta] = useState('');
+  const metaChange = useCallback((newMeta) => setMeta(newMeta), []);
+
+  // const [id, setId] = useState('');
+  // const idChange = useCallback((newId) => setId(newId), []);
+
+  // const [result, setResult] = useState('');
+  // const [accessing, setAccessing] = useState(false);
+
+  const handleSubmit = () => {
+    console.log("FormData to be submitted:", meta);
+    submit(meta, { replace: true, method: "POST" });
+    console.log("Submitted!");
+  };
+
   return (
-    <Page>
-      <ui-title-bar title="Remix app template">
-        <button variant="primary" onClick={generateProduct}>
+    <Page title ="Shopify Function（Cart and Checkout Validation Function API）">
+      <ui-title-bar title="成果課題5">
+        {/* <button variant="primary" onClick={generateProduct}>
           Generate a product
-        </button>
+        </button> */}
       </ui-title-bar>
       <VerticalStack gap="5">
         <Layout>
           <Layout.Section>
             <Card>
+              <Layout>
+                <Layout.Section>
+                  <List type="number">
+                    <List.Item>
+                      <Badge status='info'>商品</Badge>の<Link url={`https://${_getAdminFromShop(shop)}/settings/custom_data`} target="_blank">メタフィールド</Link>を作成します<br />
+                      タイプ：<Badge>整数</Badge>,　名前：<Badge>Max Amount</Badge>,　ネームスペースとキー<Badge>custom.max_amount</Badge><br />
+                      ※上記以外のネームスペースとキーの場合動作ができません。
+                      {/* <form onSubmit={handleSubmit}>
+                        <TextField
+                          label=""
+                          value={meta}
+                          onChange={setMeta}
+                          autoComplete="off"
+                          placeholder="Example: custom.max_amount"
+                        />
+                        <Button loading={isLoading} primary onClick={handleSubmit}>
+                          送信
+                        </Button>
+                      </form> */}
+                    </List.Item>
+                    <List.Item>
+                      <Link url={`https://${_getAdminFromShop(shop)}/products`} target="_blank">商品</Link>に追加されたメタフィールド：<Badge>Max amount</Badge>に数量を設定しします<br />
+                      （例：2を入力 = 2点まで購入可能）
+                    </List.Item>
+                    <List.Item>
+                      設定 ＞ <Link url={`https://${_getAdminFromShop(shop)}/settings/checkout`} target="_blank">チェックアウト</Link>からチェックアウトルールの追加を選択して<br />
+                      <Badge status='info'>cart-checkout-validation</Badge>を有効化します
+                    </List.Item>
+                    <List.Item>
+                      カートで購入数量上限のバリデーションが動作します。
+                      <img
+                        alt=""
+                        width="70%"
+                        height="70%"
+                        style={{
+                          // objectFit: 'cover',
+                          objectPosition: 'center',
+                        }}
+                        src="https://i.gyazo.com/9f2f0b144707dd285310aae75f199e4b.png"
+                      />
+                    </List.Item>
+                  </List>
+                </Layout.Section>
+              </Layout>
+            </Card>
+            {/* <Card>
               <VerticalStack gap="5">
                 <VerticalStack gap="2">
                   <Text as="h2" variant="headingMd">
@@ -178,9 +250,9 @@ export default function Index() {
                   </Box>
                 )}
               </VerticalStack>
-            </Card>
+            </Card> */}
           </Layout.Section>
-          <Layout.Section secondary>
+          {/* <Layout.Section secondary>
             <VerticalStack gap="5">
               <Card>
                 <VerticalStack gap="2">
@@ -269,7 +341,7 @@ export default function Index() {
                 </VerticalStack>
               </Card>
             </VerticalStack>
-          </Layout.Section>
+          </Layout.Section> */}
         </Layout>
       </VerticalStack>
     </Page>
